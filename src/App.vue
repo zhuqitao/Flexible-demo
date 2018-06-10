ca5fde953a8d6a54366b1c90bc21fc088cc543cb
zhuqitao <1362335135@qq.com>
2018-06-10 15:35:11 +0800

    vue-cli flexible+px2rem实例

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <div class="box1"></div>
+    <div class="box2">
+      asd
+    </div>
+    <div class="box3">asd</div>
+    <div class="box4"></div>
+    <div class="box5"></div>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<style scoped>
+.wrapper div {
+  height: 1rem;
+}
+.box1 {
+  /* 750 * 20% */
+  width: 150px;
+  background-color: coral;
+}
+.box2 {
+  /* 750 * 40% */
+  width: 300px;
+  font-size: 20px;/*px*/
+  background-color: skyblue;
+}
+.box3 {
+  /* 750 * 60% */
+  width: 450px;/*no*/
+  font-size: 30px;/*px*/
+  background-color: palegreen;
+}
+.box4 {
+  /* 750 * 80% */
+  width: 600px;
+  background-color: wheat;
+}
+.box5 {
+  /* 750 * 100% */
+  width: 750px;
+  background-color: darkred;
+}
+</style>
